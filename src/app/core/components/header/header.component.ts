70206baed2adb12570f93e98d15bac9773019d42
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-07-16 03:19:41 -0300

    alternando entre  noite e dia e impactando icons

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  
+  public icon:string = "sun"
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public toggle() {
+   const themes = document.body.classList.toggle('dark-theme')
+
+   if(themes){
+     return this.icon = 'moon'
+   }
+
+   return this.icon = 'sun'
+  }
 }
